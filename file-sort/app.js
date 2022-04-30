85ab82ebe699b8f90a5987b796f330040ba54da4
ClarXo <cheonaru@gmail.com>
2022-04-30 21:47:42 +0900

    filter files from directory

--- file-sort/app.js
+++ file-sort/app.js
@@ -6,6 +6,16 @@ const homedir = os.homedir();
 const targetDirName = process.argv[2];
 const targetDirPath = path.join(homedir, 'downloads', targetDirName);
 
-fs.mkdir(path.join(targetDirPath, 'video')).catch(console.log);
-fs.mkdir(path.join(targetDirPath, 'captured')).catch(console.log);
-fs.mkdir(path.join(targetDirPath, 'duplicated')).catch(console.log);
+function makeDirectoriesForSort() {
+  fs.mkdir(path.join(targetDirPath, 'video')).catch(console.log);
+  fs.mkdir(path.join(targetDirPath, 'captured')).catch(console.log);
+  fs.mkdir(path.join(targetDirPath, 'duplicated')).catch(console.log);
+}
+
+async function getFileListOnly(targetDirPath) {
+  const fileList = await fs.readdir(targetDirPath, { withFileTypes: true });
+  return fileList.filter((dirent) => dirent.isFile()).map((dirent) => dirent.name);
+}
+
+makeDirectoriesForSort();
+getFileListOnly(targetDirPath).then(console.log);
