4b6d31f3beb80d1a0dff42ff699d52f4df135a5c
ClarXo <cheonaru@gmail.com>
2022-05-01 17:42:21 +0900

    file sorting script

--- file-sort/app.js
+++ file-sort/app.js
@@ -1,21 +1,54 @@
 const path = require('path');
 const os = require('os');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const homedir = os.homedir();
 const targetDirName = process.argv[2];
 const targetDirPath = path.join(homedir, 'downloads', targetDirName);
 
 function makeDirectoriesForSort() {
-  fs.mkdir(path.join(targetDirPath, 'video')).catch(console.log);
-  fs.mkdir(path.join(targetDirPath, 'captured')).catch(console.log);
-  fs.mkdir(path.join(targetDirPath, 'duplicated')).catch(console.log);
+  fs.promises.mkdir(path.join(targetDirPath, 'video')).catch(console.log);
+  fs.promises.mkdir(path.join(targetDirPath, 'captured')).catch(console.log);
+  fs.promises.mkdir(path.join(targetDirPath, 'duplicated')).catch(console.log);
 }
 
 async function getFileListOnly(targetDirPath) {
-  const fileList = await fs.readdir(targetDirPath, { withFileTypes: true });
+  const fileList = await fs.promises.readdir(targetDirPath, { withFileTypes: true });
   return fileList.filter((dirent) => dirent.isFile()).map((dirent) => dirent.name);
 }
 
-makeDirectoriesForSort();
-getFileListOnly(targetDirPath).then(console.log);
+function moveFileToFolder(fileName, folderName) {
+  const originFilePath = path.join(targetDirPath, fileName);
+  const targetPath = path.join(targetDirPath, folderName, fileName);
+
+  const readStream = fs.createReadStream(originFilePath);
+  const writeStream = fs.createWriteStream(targetPath);
+
+  const piping = readStream.pipe(writeStream);
+
+  piping.on('finish', () => {
+    fs.unlink(originFilePath, () => console.log(`${fileName} moved to ${folderName}`));
+  });
+}
+
+function handleFile(fileName) {
+  switch (path.extname(fileName).toLowerCase()) {
+    case '.png':
+    case '.aae':
+      return moveFileToFolder(fileName, 'captured');
+    case '.mp4':
+    case '.mov':
+      return moveFileToFolder(fileName, 'video');
+    case '.jpg':
+      if (fileName.startsWith('IMG_E')) {
+        const originFileName = fileName.replace(/^(IMG_E)/, 'IMG_');
+        return moveFileToFolder(originFileName, 'duplicated');
+      }
+      return;
+  }
+}
+
+// makeDirectoriesForSort();
+getFileListOnly(targetDirPath).then((fileList) => {
+  fileList.forEach((file) => handleFile(file));
+});
