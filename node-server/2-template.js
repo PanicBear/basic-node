811aaba785e4ab3abfad01671323f392a15de62e
ClarXo <imp_rove@naver.com>
2022-05-07 14:21:30 +0900

    template engine - ejs

---
+++ node-server/2-template.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+
+const name = "Clarko";
+const courses = [
+  {
+    name: "HTML",
+  },
+  {
+    name: "CSS",
+  },
+  {
+    name: "JS",
+  },
+  {
+    name: "Node",
+  },
+];
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    ejs
+      .renderFile("./template/index.ejs", { name })
+      .then((data) => res.end(data));
+  } else if (url === "/courses") {
+    ejs
+      .renderFile("./template/courses.ejs", { courses })
+      .then((data) => res.end(data));
+  } else {
+    ejs
+      .renderFile("./template/not-found.ejs", { name })
+      .then((data) => res.end(data));
+  }
+});
+
+server.listen(8080);
