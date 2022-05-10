4ba510236c123f8798eabbe4762992ab234c6bc2
ClarXo <cheonaru@gmail.com>
2022-05-10 11:29:15 +0900

    middleware

--- express/app.js
+++ express/app.js
@@ -1,12 +1,35 @@
 import express from 'express';
 const app = express();
 
-app.get('/sky/:id', (req, res, next) => {
-  // console.log(req.path);
-  // console.log(req.headers);
-  console.log(req.params);
-  console.log(req.query);
-  // res.send('hi!');
-  res.status(201).send('test');
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('first1');
+    next('route');
+  },
+  (req, res, next) => {
+    console.log('first2');
+    next();
+  },
+);
+
+app.get('/', (req, res, next) => {
+  res.send('second');
+  console.log('second');
 });
+
+app.get('/error', (req, res, next) => {
+  console.log('error page');
+  next(new Error('error page'));
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Not available');
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send('Sorry, try later');
+});
+
 app.listen(8080);
