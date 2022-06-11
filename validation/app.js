98a71bef37de958d9917536f97a8287b68ee75c8
ClarXo <cheonaru@gmail.com>
2022-06-11 21:21:33 +0900

    validation - refactoring

--- validation/app.js
+++ validation/app.js
@@ -5,19 +5,14 @@ const app = express();
 
 app.use(express.json());
 
-// app.post(
-//   '/users',
-//   body('name').isLength({ min: 2 }).withMessage('fill name longer than 2 char'),
-//   body('age').isInt().withMessage('숫자를 입력해주세요'),
-//   (req, res, next) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       res.status(400).json({ message: errors.array() });
-//     }
-//     console.log(req.body);
-//     res.sendStatus(201);
-//   },
-// );
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  return res.status(400).json({ message: errors.array()[0].msg });
+};
+
 app.post(
   '/users',
   [
@@ -25,23 +20,20 @@ app.post(
     body('age').isInt().withMessage('숫자를 입력해주세요'),
     body('email').isEmail().withMessage('이메일 형식으로 입력해주세요'),
     body('jobname').notEmpty(),
+    validate,
   ],
   (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errors.array() });
-    }
     console.log(req.body);
     res.sendStatus(201);
   },
 );
 
-app.get('/:email', param('email').isEmail().withMessage('이메일 형식으로 입력해주세요'), (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ message: errors.array() });
-  }
-  res.send('test');
-});
+app.get(
+  '/:email',
+  [param('email').isEmail().withMessage('이메일 형식으로 입력해주세요'), validate],
+  (req, res, next) => {
+    res.send('test');
+  },
+);
 
 app.listen(8080);
